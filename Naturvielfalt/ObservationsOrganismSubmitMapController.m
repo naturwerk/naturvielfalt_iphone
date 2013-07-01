9a2fb805b1f7e44d3019696b92921ad8e0694975
Albert von Felten <xy>
2013-07-01 14:16:30 +0200

    accuracy display in mapView

--- Naturvielfalt/ObservationsOrganismSubmitMapController.m
+++ Naturvielfalt/ObservationsOrganismSubmitMapController.m
@@ -232,7 +232,7 @@
     } else {
         accuracyImage = red;
     }
-    accuracyText = [[NSString alloc] initWithFormat:@"%dm", accuracyValue];
+    accuracyText = [[NSString alloc] initWithFormat:@"%dm %@", accuracyValue, NSLocalizedString(@"observationAcc", nil)];
     accuracyLabel.text = accuracyText;
     accuracyImageView.image = accuracyImage;
 }

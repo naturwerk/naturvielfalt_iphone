f5e1428556647a12c67ee27246ca13daf1c6ffd4
Albert von Felten <xy>
2013-08-08 09:04:00 +0200

    bug fixed

--- Naturvielfalt/SettingsViewController.m
+++ Naturvielfalt/SettingsViewController.m
@@ -35,7 +35,10 @@
 
 #pragma mark - View lifecycle
 - (void)viewDidLoad
-{    
+{
+    
+    titlesSectionOne = [[NSMutableArray alloc] init];
+    
     [titlesSectionOne addObject:NSLocalizedString(@"settingsUsername", nil)];
     [titlesSectionOne addObject:NSLocalizedString(@"settingsPwd", nil)];
     [titlesSectionOne addObject:NSLocalizedString(@"settingsAccountInfo", nil)];
@@ -43,7 +46,7 @@
     //[titlesSectionOne addObject:NSLocalizedString(@"settingsWikiArt", nil)];
     
     /*titlesSectionTwo = [[NSMutableArray alloc] init];
-    [titlesSectionTwo addObject:NSLocalizedString(@"settingsMap", nil)];*/
+     [titlesSectionTwo addObject:NSLocalizedString(@"settingsMap", nil)];*/
     
     [super viewDidLoad];
     
@@ -73,9 +76,9 @@
     if(indexPath.row == 0) {
         // Username
         // Create the ObservationsOrganismSubmitCameraController
-        SettingsUsernameViewController *settingsUsernameController = [[SettingsUsernameViewController alloc] 
-                                                                                      initWithNibName:@"SettingsUsernameViewController" 
-                                                                                      bundle:[NSBundle mainBundle]];
+        SettingsUsernameViewController *settingsUsernameController = [[SettingsUsernameViewController alloc]
+                                                                      initWithNibName:@"SettingsUsernameViewController"
+                                                                      bundle:[NSBundle mainBundle]];
         
         // Switch the View & Controller
         [self.navigationController pushViewController:settingsUsernameController animated:YES];
@@ -83,8 +86,8 @@
     } else if(indexPath.row == 1) {
         // Password
         // Create the ObservationsOrganismSubmitCameraController
-        SettingsPasswordViewController *settingsPasswordController = [[SettingsPasswordViewController alloc] 
-                                                                      initWithNibName:@"SettingsPasswordViewController" 
+        SettingsPasswordViewController *settingsPasswordController = [[SettingsPasswordViewController alloc]
+                                                                      initWithNibName:@"SettingsPasswordViewController"
                                                                       bundle:[NSBundle mainBundle]];
         
         // Switch the View & Controller
@@ -109,7 +112,7 @@
                 }
                 
                 cell.textLabel.text = [titlesSectionOne objectAtIndex:indexPath.row];
-               
+                
                 
                 //if(indexPath.row < 2) {
                 // Set detail label
@@ -136,7 +139,7 @@
                 }
                 
                 cell.textLabel.text = [titlesSectionOne objectAtIndex:indexPath.row];
-                 cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
+                cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
                 NSString *password = @"";
                 
                 if([appSettings objectForKey:@"password"] != nil) {
@@ -169,79 +172,79 @@
         }
         
         /*} else {
-            UISwitch *mySwitch = [[UISwitch alloc] initWithFrame:CGRectZero];
-            
-            cell.accessoryView = mySwitch;
-            
-            if(indexPath.row == 2) {
-                
-                if([appSettings objectForKey:@"showImages"] != nil) {
-                    BOOL showImages = [[appSettings stringForKey:@"showImages"] isEqualToString:@"on"];
-                    [(UISwitch *)cell.accessoryView setOn:showImages];
-                }
-                
-            }
-            if(indexPath.row == 3) {
-                
-                if([appSettings objectForKey:@"showWikipedia"] != nil) {
-                    BOOL showWikipedia = [[appSettings stringForKey:@"showWikipedia"] isEqualToString:@"on"];
-                    [(UISwitch *)cell.accessoryView setOn:showWikipedia];
-                }
-                
-            }
-            [(UISwitch *)cell.accessoryView addTarget:self action:@selector(mySelector:)
-                                    forControlEvents:UIControlEventValueChanged];
-        }
-    } else {
-        
-        if(cell == nil) {
-            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-        }
-        
-        NSArray *keys = [NSArray arrayWithObjects:NSLocalizedString(@"settingsMapSatellite", nil), NSLocalizedString(@"settingsMapHybrid", nil), NSLocalizedString(@"settingsMapStandard", nil), nil];
-        segmentControl = [[UISegmentedControl alloc] initWithItems:keys];
-        [segmentControl setSegmentedControlStyle:UISegmentedControlStyleBar];
-        segmentControl.frame = CGRectMake(10, 0, segmentControl.frame.size.width, segmentControl.frame.size.height);
-        
-        [segmentControl addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
-        
+         UISwitch *mySwitch = [[UISwitch alloc] initWithFrame:CGRectZero];
+         
+         cell.accessoryView = mySwitch;
+         
+         if(indexPath.row == 2) {
+         
+         if([appSettings objectForKey:@"showImages"] != nil) {
+         BOOL showImages = [[appSettings stringForKey:@"showImages"] isEqualToString:@"on"];
+         [(UISwitch *)cell.accessoryView setOn:showImages];
+         }
+         
+         }
+         if(indexPath.row == 3) {
+         
+         if([appSettings objectForKey:@"showWikipedia"] != nil) {
+         BOOL showWikipedia = [[appSettings stringForKey:@"showWikipedia"] isEqualToString:@"on"];
+         [(UISwitch *)cell.accessoryView setOn:showWikipedia];
+         }
+         
+         }
+         [(UISwitch *)cell.accessoryView addTarget:self action:@selector(mySelector:)
+         forControlEvents:UIControlEventValueChanged];
+         }
+         } else {
+         
+         if(cell == nil) {
+         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+         }
+         
+         NSArray *keys = [NSArray arrayWithObjects:NSLocalizedString(@"settingsMapSatellite", nil), NSLocalizedString(@"settingsMapHybrid", nil), NSLocalizedString(@"settingsMapStandard", nil), nil];
+         segmentControl = [[UISegmentedControl alloc] initWithItems:keys];
+         [segmentControl setSegmentedControlStyle:UISegmentedControlStyleBar];
+         segmentControl.frame = CGRectMake(10, 0, segmentControl.frame.size.width, segmentControl.frame.size.height);
+         
+         [segmentControl addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
+         
          NSUserDefaults* appSettings = [NSUserDefaults standardUserDefaults];
-        int mapType = [[appSettings stringForKey:@"mapType"] integerValue];
-        
-        switch (mapType) {
-            case 1:
-            {
-                [segmentControl setSelectedSegmentIndex:0];
-                break;
-            }
-            case 2:
-            {
-                [segmentControl setSelectedSegmentIndex:1];
-                break;
-            }
-            case 3:
-            {
-                [segmentControl setSelectedSegmentIndex:2];
-                break;
-            }
-        }
-        
-        //[cell setBackgroundColor: [UIColor colorWithWhite:1.0f alpha:0.0f]];
-        cell.backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
-        [cell addSubview:segmentControl];*/
+         int mapType = [[appSettings stringForKey:@"mapType"] integerValue];
+         
+         switch (mapType) {
+         case 1:
+         {
+         [segmentControl setSelectedSegmentIndex:0];
+         break;
+         }
+         case 2:
+         {
+         [segmentControl setSelectedSegmentIndex:1];
+         break;
+         }
+         case 3:
+         {
+         [segmentControl setSelectedSegmentIndex:2];
+         break;
+         }
+         }
+         
+         //[cell setBackgroundColor: [UIColor colorWithWhite:1.0f alpha:0.0f]];
+         cell.backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+         [cell addSubview:segmentControl];*/
     }
     return nil;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-
+    
     /*NSString * text = [titlesSectionOne objectAtIndex:indexPath.row];
-    CGSize testSize = CGSizeMake(tableView.frame.size.width, tableView.frame.size.height - 50);
-    CGSize textSize = [text sizeWithFont:[UIFont systemFontOfSize: 14.0] forWidth:[tableView frame].size.width - 40.0 lineBreakMode:UILineBreakModeWordWrap];
-    // return either default height or height to fit the text
-    CGFloat res = textSize.height < 44.0 ? 44.0 : textSize.height;
-    NSLog(@"heightForRowAtIndexPath: textSize.height - %f float - %f", textSize.height, res);
-    return testSize.height < 44.0 ? 44.0 : textSize.height;*/
+     CGSize testSize = CGSizeMake(tableView.frame.size.width, tableView.frame.size.height - 50);
+     CGSize textSize = [text sizeWithFont:[UIFont systemFontOfSize: 14.0] forWidth:[tableView frame].size.width - 40.0 lineBreakMode:UILineBreakModeWordWrap];
+     // return either default height or height to fit the text
+     CGFloat res = textSize.height < 44.0 ? 44.0 : textSize.height;
+     NSLog(@"heightForRowAtIndexPath: textSize.height - %f float - %f", textSize.height, res);
+     return testSize.height < 44.0 ? 44.0 : textSize.height;*/
     if (indexPath.row == 2) {
         return 140.0;
     }
@@ -249,60 +252,60 @@
 }
 
 /*- (IBAction)segmentChanged:(id)sender {
-    NSUserDefaults* appSettings = [NSUserDefaults standardUserDefaults];
-    switch (segmentControl.selectedSegmentIndex) {
-        case 0:
-        {
-            NSLog(@"satelite");
-            [appSettings setObject:@"1" forKey:@"mapType"];
-            break;
-        }
-        case 1:
-        {
-            NSLog(@"hybride");
-            [appSettings setObject:@"2" forKey:@"mapType"];
-            break;
-        }
-        case 2:
-        {
-            NSLog(@"map");
-            [appSettings setObject:@"3" forKey:@"mapType"];
-        }
-    }
-    [appSettings synchronize];
-}*/
+ NSUserDefaults* appSettings = [NSUserDefaults standardUserDefaults];
+ switch (segmentControl.selectedSegmentIndex) {
+ case 0:
+ {
+ NSLog(@"satelite");
+ [appSettings setObject:@"1" forKey:@"mapType"];
+ break;
+ }
+ case 1:
+ {
+ NSLog(@"hybride");
+ [appSettings setObject:@"2" forKey:@"mapType"];
+ break;
+ }
+ case 2:
+ {
+ NSLog(@"map");
+ [appSettings setObject:@"3" forKey:@"mapType"];
+ }
+ }
+ [appSettings synchronize];
+ }*/
 
 /*- (void) mySelector:(id)sender {
-    // Cast the sender as a UISwitch
-    UISwitch *aSwitch = (UISwitch *)sender;
-    
-    // Cast the superview of aSwitch to a UITableViewCell
-    UITableViewCell *cell = (UITableViewCell *)aSwitch.superview;
-    
-    // You could get an indexPath as follows (though you don't need it in this case)
-    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
-    NSUserDefaults* appSettings = [NSUserDefaults standardUserDefaults];
-    NSString *on = (aSwitch.on) ? @"on" : @"off";
-    
-    if(indexPath.row == 2) {
-        
-        // Show images value
-        [appSettings setObject:on forKey:@"showImages"];
-    }
-    if(indexPath.row == 3) {
-        
-        // Show images value
-        [appSettings setObject:on forKey:@"showWikipedia"];
-    }
-    [appSettings synchronize];
-}*/
+ // Cast the sender as a UISwitch
+ UISwitch *aSwitch = (UISwitch *)sender;
+ 
+ // Cast the superview of aSwitch to a UITableViewCell
+ UITableViewCell *cell = (UITableViewCell *)aSwitch.superview;
+ 
+ // You could get an indexPath as follows (though you don't need it in this case)
+ NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+ NSUserDefaults* appSettings = [NSUserDefaults standardUserDefaults];
+ NSString *on = (aSwitch.on) ? @"on" : @"off";
+ 
+ if(indexPath.row == 2) {
+ 
+ // Show images value
+ [appSettings setObject:on forKey:@"showImages"];
+ }
+ if(indexPath.row == 3) {
+ 
+ // Show images value
+ [appSettings setObject:on forKey:@"showWikipedia"];
+ }
+ [appSettings synchronize];
+ }*/
 
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     //if (section == 0) {
-        return [titlesSectionOne count];
+    return [titlesSectionOne count];
     /*} else {
-        return [titlesSectionTwo count];
-    }*/
+     return [titlesSectionTwo count];
+     }*/
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -311,8 +314,8 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     /*if (section == 1) {
-        return NSLocalizedString(@"settingsMap", nil);
-    }*/
+     return NSLocalizedString(@"settingsMap", nil);
+     }*/
     return nil;
 }
 
